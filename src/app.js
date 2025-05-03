bc334b4f6bee6a436b8820c6849b6faa24b0ff78
JosielSantos16 <josielufopa@gmail.com>
2025-05-03 10:38:09 -0300

    tentando subir 2

--- src/app.js
+++ src/app.js
@@ -1,32 +1,33 @@
-import express from 'express';
-import cors from 'cors';
-import './database/index.js';            
-import { resolve } from 'path';
-import routes from './routes.js';
+import express from 'express'
+import routes from './routes';
+import cors from 'cors'
+import './database'
+import { resolve } from 'path'
 
-class App {
-  constructor() {
-    this.app = express();
-    this.app.use(cors());
-    this.middlewares();
-    this.routes();
-  }
+class App{
+    constructor(){
+        this.app = express();
+        this.app.use(cors())
+        this.middlewares();
+        this.routes();
+        
+    }
 
-  middlewares() {
-    this.app.use(express.json());
-    this.app.use(
-      '/product-file',
-      express.static(resolve(__dirname, '..', 'uploads'))
-    );
-    this.app.use(
-      '/category-file',
-      express.static(resolve(__dirname, '..', 'uploads'))
-    );
-  }
+    middlewares(){
+        this.app.use(express.json());
+        this.app.use(
+            '/product-file',
+            express.static(resolve(__dirname, '..', 'uploads'))
+        )
+        this.app.use(
+            '/category-file',
+            express.static(resolve(__dirname, '..', 'uploads'))
+        )
+    }
 
-  routes() {
-    this.app.use(routes);
-  }
+    routes(){
+        this.app.use(routes)
+    }
 }
 
-export default new App().app;
+export default new App().app
